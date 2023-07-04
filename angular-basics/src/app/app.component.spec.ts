28cec187ead3d301f2ce54cd3ef34771d576a0f0
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-07-04 21:36:56 +0530

    Added new component named server

--- angular-basics/src/app/app.component.spec.ts
+++ angular-basics/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-basics'`, () => {
+  it(`should have as title 'my-first-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-basics');
+    expect(app.title).toEqual('my-first-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-basics app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-app app is running!');
   });
 });
