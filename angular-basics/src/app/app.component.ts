4aa96aa2ca7f3d195c64159a78349ef797a11206
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-07-04 21:44:33 +0530

    Implemented different styles for Component Selectors

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles : [`
-  h1 {
-    color : dodgerblue;
-  }
-  `]
+  styleUrls: ['./app.component.css']
+  // styles : [`
+  // h1 {
+  //   color : dodgerblue;
+  // }
+  // `]
 })
 export class AppComponent {
   title = 'my-first-app';
--- angular-basics/src/app/servers/servers.component.ts
+++ angular-basics/src/app/servers/servers.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //app-servers as element style
   selector: 'app-servers',
+  
+  //app-servers as attribute style
+  // selector: '[app-servers]',
+  
+  //app-servers as class style
+  //selector: '.app-servers',
   template: `
   <app-server></app-server>
   <app-server></app-server>
