957465ae9940b8f1e016734d57cf4d00ddba90f0
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-07-05 12:55:43 +0530

    Added implementation to cover all forms of databinding

--- angular-basics/src/app/servers/servers.component.ts
+++ angular-basics/src/app/servers/servers.component.ts
@@ -28,7 +28,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created . Name is ' + this.serverName;
     return this.serverCreationStatus;
   }
 
